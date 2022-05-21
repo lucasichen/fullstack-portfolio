63e19db8a29504437b2f86c2c11beb1ce31c160e
Lucas Chen <lucas.chen2016@gmail.com>
2022-05-21 19:46:26 -0400

    Might fix netlify form

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -47,7 +47,7 @@ const Footer = () => {
       </div>
       
       {!isSubmitted ?
-      <form className='app__footer-form app__flex' data-netlify="true" method="POST">
+      <form className='app__footer-form app__flex' data-netlify="true" method="POST" name='contact'>
         <div className='app__flex'>
           <input type='text' placeholder='Name' className='p-text' name='name' value={name} onChange={handleChangeInput}/>
         </div>

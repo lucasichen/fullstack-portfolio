db8ddf39f1dde273c769e83ef8565dea2f3fe4a5
Lucas Chen <lucas.chen2016@gmail.com>
2023-09-29 16:40:00 -0400

    fix: change order of technologies and add typecsript

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -56,7 +56,7 @@ const Header = () => {
       whileInView={scaleVariants.whileInView}
       className='app__header-circles'
       >
-        {[images.javascript, images.python, images.react].map((circle, index) => (
+        {[images.typescript, images.react, images.python].map((circle, index) => (
           <div className='circle-cmp app__flex' key={`circle-${index}`}>
             <img src={circle} alt='circle'/>
           </div>
